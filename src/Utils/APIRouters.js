2781ed877e20f81df8f14dd081f632fe16f45a21
Manith kumar <117072043+Manithkumar016@users.noreply.github.com>
2023-07-27 10:45:35 +0530

    Update APIRouters.js

--- src/Utils/APIRouters.js
+++ src/Utils/APIRouters.js
@@ -1,4 +1,4 @@
-export const host = "https://drab-gold-chicken-kilt.cyclic.app";
+export const host = "https://frantic-stole-hen.cyclic.app";
 
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
